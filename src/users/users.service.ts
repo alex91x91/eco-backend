bcaa2a86a073531a35a4d666cc13f9870ea1ee1c
Aliaksandr <alexinforest@gmail.com>
2025-02-02 13:59:32 +0300

    local and jwt strategies

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,9 +1,35 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnprocessableEntityException } from '@nestjs/common';
 import { CreateUserRequest } from './dto/create-user.request';
-
+import { PrismaService } from 'src/prisma/prisma.service';
+import * as bcrypt from 'bcrypt';
+import { Prisma } from '@prisma/client';
 @Injectable()
 export class UsersService {
-  createUser(data: CreateUserRequest) {
-    console.log(data);
+  constructor(private readonly prismaService: PrismaService) {}
+
+  async createUser(data: CreateUserRequest) {
+    try {
+      return await this.prismaService.user.create({
+        data: {
+          ...data,
+          password: await bcrypt.hash(data.password, 10),
+        },
+        select: {
+          email: true,
+          id: true,
+        },
+      });
+    } catch (error) {
+      if (error.code === 'P2002') {
+        throw new UnprocessableEntityException('Email already exists.');
+      }
+      throw error;
+    }
+  }
+
+  async getUser(filter: Prisma.UserWhereUniqueInput) {
+    return this.prismaService.user.findUniqueOrThrow({
+      where: filter,
+    });
   }
 }
